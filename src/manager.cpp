c9986eeb46c52586904d9981f173588cb38f49ee
Arya K Padman <aryakpadman@gmail.com>
2025-06-02 21:22:09 -0500

    Replace `std::system` with `posix_spawn` for async execution
    This commit introduces the changes to replace the `std::system` with
    `posix_spawn` for the execution of RSYNC CLI in order to achieve the
    asynchronous execution of multiple RSYNC commands.
    
    Reasons for moving to posix_spawn():
    
     - The current `std::system()` is a blocking call where it hold until
       the command finishes execution which is achieving asynchronous
       behavior.
     - `std::system()` lacks the method to capture the output of the
       execution to the parent/caller and the return value of the
       execution which results in limited error handling.
     - `posix_spawn` enables launching child processes where the commands
       can be executed. The parent will wait until the command execution
       completes, but asynchronously by waiting the child to write the
       output into the write end of the pipe and parent reads the same.
     - `posix_spawn_file_actions` allows fine-grained control of fd
       redirection, useful for isolating command input/output.
    
    Details of implementation:
    
     - Introduced a class `AsyncCommandExecutor` which implements the
       required logic to execute the CLI commands asynchronously.
     - The member function `execCmd()` invokes `posix_spawn()` to execute
       the rsync command asynchronously.
     - Redirects child's stdout/stderr through a pipe to capture and process
       output at the parent.
     - Introduced another class `SpawnActions` which initializes
       `posix_spawn_file_actions_t` inorder safely setup and destroy file
       descriptors.
     - The API `waitForCmdCompletion()` waits on the child process and reads
       output from the pipe.
     - The `execCmd()` finally returns the result code and output string to
       the caller.
    
    Additional changes :
     - Introducing the fix for the failure of existing testcases.
     - The data-sync using rsync with --relative, it reconstructs
       the source path tree and attempts to create destination directory
       on every rsync. The first call succeeds, but subsequent calls may
       fail with "File exists (17)". To avoid this, ensure the destination
       directory is created beforehand, or use a different destination path
     - Wait to ensure the immediate and periodic sync tasks configured
       in the test setup are spawned
    
    Tested:
     - Verified the sync behavior on Huygens simics model during full sync
       and immediate sync.
    
    Change-Id: I2f80f7806db62a022425df9e84a7edeb28d355e9
    Signed-off-by: Arya K Padman <aryakpadman@gmail.com>

--- src/manager.cpp
+++ src/manager.cpp
@@ -2,7 +2,11 @@
 
 #include "manager.hpp"
 
+#include "async_command_exec.hpp"
 #include "data_watcher.hpp"
+
+#include <fcntl.h>
+#include <spawn.h>
 
 #include <nlohmann/json.hpp>
 #include <phosphor-logging/lg2.hpp>
@@ -141,8 +145,8 @@ sdbusplus::async::task<> Manager::startSyncEvents()
             {
                 setSyncEventsHealth(SyncEventsHealth::Critical);
                 lg2::error(
-                    "Failed to configure immediate sync for {PATH}: {EXC}",
-                    "EXC", e, "PATH", dataSyncCfg._path);
+                    "Failed to configure immediate sync for {PATH}: {EXCEPTION}",
+                    "EXCEPTION", e, "PATH", dataSyncCfg._path);
             }
         }
         else if (dataSyncCfg._syncType == Periodic)
@@ -155,17 +159,14 @@ sdbusplus::async::task<> Manager::startSyncEvents()
             {
                 setSyncEventsHealth(SyncEventsHealth::Critical);
                 lg2::error("Failed to configure periodic sync for {PATH}: "
-                           "{EXC}",
-                           "EXC", e, "PATH", dataSyncCfg._path);
+                           "{EXCEPTION}",
+                           "EXCEPTION", e, "PATH", dataSyncCfg._path);
             }
         }
     });
     co_return;
 }
 
-// TODO: This isn't truly an async operation — Need to use popen/posix_spawn to
-// run the rsync command asynchronously but it will be handled as part of
-// concurrent sync changes.
 sdbusplus::async::task<bool>
     // NOLINTNEXTLINE
     Manager::syncData(const config::DataSyncConfig& dataSyncCfg,
@@ -202,9 +203,10 @@ sdbusplus::async::task<bool>
     // Add destination data path if configured
     syncCmd.append(dataSyncCfg._destPath.value_or(fs::path("")));
     lg2::debug("RSYNC CMD : {CMD}", "CMD", syncCmd);
-    int result = std::system(syncCmd.c_str()); // NOLINT
 
-    if (result != 0)
+    data_sync::async::AsyncCommandExecutor executor(_ctx);
+    auto result = co_await executor.execCmd(syncCmd); // NOLINT
+    if (result.first != 0)
     {
         // TODOs:
         // 1. Retry based on rsync error code
@@ -217,7 +219,8 @@ sdbusplus::async::task<bool>
         // implemented.
         // setSyncEventsHealth(SyncEventsHealth::Critical);
 
-        lg2::error("Error syncing: {PATH}", "PATH", dataSyncCfg._path);
+        lg2::error("Error syncing [{PATH}], Error : {ERROR}", "PATH",
+                   dataSyncCfg._path, "ERROR", result.second);
 
         co_return false;
     }
@@ -377,7 +380,9 @@ sdbusplus::async::task<void> Manager::startFullSync()
         }
         catch (const std::exception& e)
         {
-            lg2::error("Spawn failed for full sync {EXC}", "EXC", e);
+            lg2::error(
+                "Full sync spawn failed for [{PATH}], Error : {EXCEPTION}",
+                "PATH", cfg._path, "EXCEPTION", e);
             setFullSyncStatus(FullSyncStatus::FullSyncFailed);
         }
     }
